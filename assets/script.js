0f50d6720199a1393e42d90ccc649e148ef66c92
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-22 00:02:29 +0100

    changes

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+//https://api.open-meteo.com/v1/forecast?latitude=50.4108095&longitude=4.444643&current=temperature_2m,wind_speed_10m,weather_code&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code
+
 //https://open-meteo.com/en/docs#latitude=50.4114&longitude=4.4445&hourly=temperature_2m,weather_code&daily=
 
 //https://api.open-meteo.com/v1/forecast?latitude=50.4114&longitude=4.4445&hourly=temperature_2m,weather_code
@@ -28,85 +30,136 @@ function displayWeather(){
 
 async function getCoordinates(cityName){
     try{
-        let response = await fetch ("https://api.positionstack.com/v1/forward?access_key=ae54919cf026e2c6e78b7fd29d118521&query=" + cityName);
+        let response = await fetch ("http://api.positionstack.com/v1/forward?access_key=ae54919cf026e2c6e78b7fd29d118521&query=" + cityName);
         let coordinates = await response.json();
         let lat = coordinates.data[0].latitude;
         let long = coordinates.data[0].longitude;
 
-        searchWeather(lat, long);
+        searchWeather(lat, long, cityName);
     } catch (error){
         console.log(error);
     }
 }
 
 
-async function searchWeather(latitude, longitude){
+async function searchWeather(latitude, longitude, cityName){
 
     try{
     let response = await fetch ("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current=temperature_2m,wind_speed_10m,weather_code&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code");
     let city = await response.json();
-    displayImage(city.current.weather_code);
+
+    let todayWeather = document.createElement("div");
+    todayWeather.className = "realTimeWeather";
+    document.querySelector("main").appendChild(todayWeather);
+
+    displayImage(city.current.weather_code, todayWeather);
+    
+    let title = document.createElement("h2");
+    let titleContent = document.createTextNode(cityName);
+    title.appendChild(titleContent);
+    document.querySelector(".realTimeWeather").prepend(title);
+    
+
     let para = document.createElement("p");
-    let paraContent = document.createTextNode(city.current.temperature_2m + "°C");
+    let paraContent = document.createTextNode(Math.round(city.current.temperature_2m) + "°C");
     para.appendChild(paraContent);
-    document.querySelector("main").appendChild(para);    
+    document.querySelector(".realTimeWeather").appendChild(para);
+
+    displayTodayData(city);
+    displayWeekForecast(city);
 
     } catch (error){
         console.log(error);
     }
 }
 
-function displayImage(code){
+function displayImage(code, element){
     if (code < 1){
-        displayIcon("clear");
+        displayIcon("clear", "Clear", element);
     } else if (code < 3){
-        displayIcon("partly_cloudy");
+        displayIcon("partly_cloudy", "Partly cloudy", element);
     } else if (code < 9){
-        displayIcon("cloudy");
+        displayIcon("cloudy", "Cloudy", element);
     } else if (code < 14){
-        displayIcon("foggy");
+        displayIcon("foggy", "Foggy", element);
     } else if (code < 20){
-        displayIcon("windy");
+        displayIcon("windy", "Windy", element);
     } else if (code < 22){
-        displayIcon("rainy");
+        displayIcon("rainy", "Rainy", element);
     } else if (code < 25){
-        displayIcon("snowy");
+        displayIcon("snowy", "Snowy", element);
     } else if (code < 26){
-        displayIcon("heavy_rain");
+        displayIcon("heavy_rain", "Heavy rain", element);
     } else if (code < 28){
-        displayIcon("heavy_snow");
+        displayIcon("heavy_snow", "Heavy snow", element);
     } else if (code < 29){
-        displayIcon("foggy");
+        displayIcon("foggy", "Foggy", element);
     } else if (code < 30){
-        displayIcon("thunderstorm");
+        displayIcon("thunderstorm", "Thunderstorm", element);
     } else if (code > 39 && code < 50){
-        displayIcon("foggy");
+        displayIcon("foggy", "Foggy", element);
     } else if (code > 49 && code < 56){
-        displayIcon("partly_cloudy");
+        displayIcon("partly_cloudy", "Partly cloudy", element);
     } else if (code > 55 && code < 70){
-        displayIcon("rainy");
+        displayIcon("rainy", "Rainy", element);
     } else if (code > 69 && code < 77){
-        displayIcon("snowy");
+        displayIcon("snowy", "Snowy", element);
     } else if (code > 76 && code < 80){
-        displayIcon("snow_grains");
+        displayIcon("snow_grains", "Snow grains", element);
     } else if (code > 79 && code < 85){
-        displayIcon("heavy_rain");
+        displayIcon("heavy_rain", "Heavy rain", element);
     } else if (code > 84 && code < 91){
-        displayIcon("heavy_snow");
+        displayIcon("heavy_snow", "Heavy snow", element);
     } else if (code > 90 && code < 93){
-        displayIcon("rainy");
+        displayIcon("rainy", "Rain", element);
     } else if (code > 92 && code < 95){
-        displayIcon("snowy");
+        displayIcon("snowy", "Snowy", element);
     } else if (code > 94 && code < 100){
-        displayIcon("thunderstorm");
+        displayIcon("thunderstorm", "Thunderstorm", element);
     } else {
-        displayIcon("partly_cloudy");
+        displayIcon("partly_cloudy", "Partly cloudy", element);
     }
 }
 
-function displayIcon(state){
+function displayIcon(state_code, state, element){
+    let title = document.createElement("h3");
+    let titleContent = document.createTextNode(state);
+    title.appendChild(titleContent);
+    element.appendChild(title);
+
     let image = document.createElement("img");
-    image.src = "assets/resources/pictures/" + state + ".svg";
+    image.src = "assets/resources/pictures/" + state_code + ".svg";
     image.className = "weatherIcons";
-    document.querySelector("main").appendChild(image);
+    element.appendChild(image);
+}
+
+function displayTodayData(weatherObject){
+    let todayData = document.createElement("div");
+    let morning = document.createElement("span");
+    let noon = document.createElement("span");
+    let evening = document.createElement("span");
+
+    let title = document.createElement("h2");
+    let titleContent = document.createTextNode("Today's forecast");
+    title.appendChild(titleContent);
+    document.querySelector('main').appendChild(title);
+
+    document.querySelector('main').appendChild(todayData);
+    todayData.appendChild(morning);
+    todayData.appendChild(noon);
+    todayData.appendChild(evening);
+    todayData.className = "todayWeather";
+
+    let morningCode = weatherObject.hourly.weather_code[6];
+    displayImage(morningCode, morning);
+
+    let noonCode = weatherObject.hourly.weather_code[12];
+    displayImage(noonCode, noon);
+
+    let eveningCode = weatherObject.hourly.weather_code[18];
+    displayImage(eveningCode, evening);
+}
+
+function displayWeekForecast(weatherObject){
+    
 }
