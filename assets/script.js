ae92709034e0b51cd3db88026cd85981eadcba66
Denis Mathieu <denismathieu1203@gmail.com>
2024-02-04 16:31:34 +0100

    fixed API

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@ let submitButton = document.querySelector("#submitButton");
 
 submitButton.addEventListener('click', (event) =>{
     event.preventDefault();
+    document.querySelector("main").innerHTML = "";
     displayWeather();
 });
 
@@ -30,10 +31,10 @@ function displayWeather(){
 
 async function getCoordinates(cityName){
     try{
-        let response = await fetch ("http://api.positionstack.com/v1/forward?access_key=ae54919cf026e2c6e78b7fd29d118521&query=" + cityName);
+        let response = await fetch ("https://us1.locationiq.com/v1/search?key=pk.bf7be254176f5f8daa0b5262992b2793&q=" + cityName + "%20&format=json");
         let coordinates = await response.json();
-        let lat = coordinates.data[0].latitude;
-        let long = coordinates.data[0].longitude;
+        let lat = coordinates[0].lat;
+        let long = coordinates[0].lon;
 
         searchWeather(lat, long, cityName);
     } catch (error){
@@ -41,6 +42,16 @@ async function getCoordinates(cityName){
     }
 }
 
+async function searchImage(cityImage, cityName){
+    try{
+        let response = await fetch ("https://api.unsplash.com/search/photos?client_id=0rzAxVwUjzKGIYfQag6cj_NJKM5Qz51wO9n5E-IZ_xs&query=" + cityName);
+        let image = await response.json();
+        cityImage.src = image.results[0].urls.raw;
+        
+    } catch (error){
+        console.log(error);
+    }
+}
 
 async function searchWeather(latitude, longitude, cityName){
 
@@ -54,10 +65,15 @@ async function searchWeather(latitude, longitude, cityName){
 
     displayImage(city.current.weather_code, todayWeather);
     
+    let cityImage = document.createElement("img");
+    searchImage(cityImage, cityName);
+    document.querySelector(".realTimeWeather").prepend(cityImage);
+
     let title = document.createElement("h2");
     let titleContent = document.createTextNode(cityName);
     title.appendChild(titleContent);
     document.querySelector(".realTimeWeather").prepend(title);
+    
     
 
     let para = document.createElement("p");
@@ -150,16 +166,86 @@ function displayTodayData(weatherObject){
     todayData.appendChild(evening);
     todayData.className = "todayWeather";
 
+    let titleMorning = document.createElement("h2");
+    let titleMorningContent = document.createTextNode("6h");
+    titleMorning.appendChild(titleMorningContent);
+    morning.appendChild(titleMorning);
     let morningCode = weatherObject.hourly.weather_code[6];
     displayImage(morningCode, morning);
+    displayWeatherInformations(weatherObject, morning, 6);
 
+    let titleNoon = document.createElement("h2");
+    let titleNoonContent = document.createTextNode("12h");
+    titleNoon.appendChild(titleNoonContent);
+    noon.appendChild(titleNoon);
     let noonCode = weatherObject.hourly.weather_code[12];
     displayImage(noonCode, noon);
+    displayWeatherInformations(weatherObject, noon, 12);
 
+    let titleEvening = document.createElement("h2");
+    let titleEveningContent = document.createTextNode("18h");
+    titleEvening.appendChild(titleEveningContent);
+    evening.appendChild(titleEvening);
     let eveningCode = weatherObject.hourly.weather_code[18];
     displayImage(eveningCode, evening);
+    displayWeatherInformations(weatherObject, evening, 18);
+}
+
+function displayWeatherInformations(city, element, hour){
+    let temperature = document.createElement("p");
+    let temperatureContent = document.createTextNode(Math.round(city.hourly.temperature_2m[hour]) + "°C");
+    temperature.appendChild(temperatureContent);
+    element.appendChild(temperature);
 }
 
 function displayWeekForecast(weatherObject){
-    
+    let weekForecast = document.createElement("div");
+    weekForecast.className = "weekForecast";
+    let day1 = document.createElement("span");
+    day1.className = "weekForecastDay";
+    let day2 = document.createElement("span");
+    day2.className = "weekForecastDay";
+    let day3 = document.createElement("span");
+    day3.className = "weekForecastDay";
+    let day4 = document.createElement("span");
+    day4.className = "weekForecastDay";
+    let day5 = document.createElement("span");
+    day5.className = "weekForecastDay";
+
+    let title = document.createElement("h2");
+    let titleContent = document.createTextNode("Week's forecast");
+    title.appendChild(titleContent);
+    document.querySelector('main').appendChild(title);
+
+    document.querySelector("main").appendChild(weekForecast);
+
+    weekForecast.appendChild(day1);
+    weekForecast.appendChild(day2);
+    weekForecast.appendChild(day3);
+    weekForecast.appendChild(day4);
+    weekForecast.appendChild(day5);
+
+    let now = new Date();
+    let today = now.getDay();
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    displayDayWeather(weekDays[(today+1)%7], day1, weatherObject, (12+(24*1)));
+
+    displayDayWeather(weekDays[(today+2)%7], day2, weatherObject, (12+(24*2)));
+
+    displayDayWeather(weekDays[(today+3)%7], day3, weatherObject, (12+(24*3)));
+
+    displayDayWeather(weekDays[(today+4)%7], day4, weatherObject, (12+(24*4)));
+
+    displayDayWeather(weekDays[(today+5)%7], day5, weatherObject, (12+(24*5)));
+}
+
+function displayDayWeather(name, element, weatherObject, time){
+    let title = document.createElement("h2");
+    let titleContent = document.createTextNode(name);
+    title.appendChild(titleContent);
+    element.appendChild(title);
+    let code = weatherObject.hourly.weather_code[time];
+    displayImage(code, element);
+    displayWeatherInformations(weatherObject, element, time);
 }
